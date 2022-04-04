954d8d4c7cbdcf14b54bc69ab2019d632f622a8a
Adam Bublavý <bublavy.adam@gmail.com>
2022-04-04 14:08:54 +0200

    Remove: Unused imports

--- app/Controllers/Http/ChannelsController.ts
+++ app/Controllers/Http/ChannelsController.ts
@@ -3,7 +3,6 @@ import { rules, schema } from '@ioc:Adonis/Core/Validator'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Channel from 'App/Models/Channel'
 import User from 'App/Models/User'
-import databaseConfig from 'Config/database'
 
 export enum ChannelTypes {
   Public = 'PUBLIC',
