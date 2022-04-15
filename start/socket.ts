d52b3c09711f1ed52480b826d1a53293865dc8df
Adam Bublavy <bublavy.adam@gmail.com>
2022-04-15 19:22:01 +0200

    Fix: Removed duplicate Ws event handler

--- start/socket.ts
+++ start/socket.ts
@@ -9,18 +9,6 @@
 
 import Ws from '@ioc:Ruby184/Socket.IO/Ws'
 
-Ws.namespace('/')
-  .connected(({ socket }) => {
-    console.log('new websocket connection: ', socket.id)
-  })
-  .disconnected(({ socket }, reason) => {
-    console.log('websocket disconnecting: ', socket.id, reason)
-  })
-  .on('hello', ({ socket }, msg: string) => {
-    console.log('websocket greeted: ', socket.id, msg)
-    return 'hi'
-  })
-
 // this is dynamic namespace, in controller methods we can use params.name
 Ws.namespace('channels/:name')
   // .middleware('channel') // check if user can join given channel
