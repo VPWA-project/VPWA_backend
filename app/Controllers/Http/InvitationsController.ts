f6dfd9be59f89aaabdf626619356cccab308c27e
Adam Bublavy <bublavy.adam@gmail.com>
2022-05-08 15:53:41 +0200

    Update: Resolve invitations via Ws

--- app/Controllers/Http/InvitationsController.ts
+++ app/Controllers/Http/InvitationsController.ts
@@ -1,13 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { schema } from '@ioc:Adonis/Core/Validator'
-import Database from '@ioc:Adonis/Lucid/Database'
 import Invitation from 'App/Models/Invitation'
 import User from 'App/Models/User'
-
-enum InvitationStatus {
-  Accept = 'ACCEPT',
-  Decline = 'DECLINE',
-}
 
 export default class InvitationsController {
   /**
@@ -30,48 +23,6 @@ export default class InvitationsController {
       sended: sendedInvitations,
       received: receivedInvitations,
     })
-  }
-
-  /**
-   * Accepts or declines invitation
-   */
-  public async resolve({ auth, request, response, params: { id } }: HttpContextContract) {
-    const validationSchema = schema.create({
-      status: schema.enum(Object.values(InvitationStatus)),
-    })
-
-    const data = await request.validate({ schema: validationSchema })
-
-    const trx = await Database.transaction()
-
-    try {
-      const invitation = await Invitation.findOrFail(id, { client: trx })
-
-      if (!invitation) {
-        return response.badRequest('Invitation not found')
-      }
-
-      const user = auth.user as User
-
-      if (invitation.userId !== user.id) {
-        return response.badRequest('Invitation does not belongs to you')
-      }
-
-      if (data.status === InvitationStatus.Accept) {
-        // add user to channel
-        await user.related('channels').attach([invitation.channelId], trx)
-      }
-
-      await invitation.delete()
-
-      await trx.commit()
-
-      return response.ok({})
-    } catch (err) {
-      await trx.rollback()
-
-      return response.abort(err)
-    }
   }
 
   /**
